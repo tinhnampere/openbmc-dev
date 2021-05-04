49785aec375e50136b52a5fe36c97fe80b06eb2a
Chanh Nguyen <chanh@os.amperecomputing.com>
2021-05-04 18:09:11 +0700

    meta-ampere: psu-inventory-update: Support CHICONY PSUs
    Update the psu-inventory-update.sh script to handle inventory
    for the CHICONY PSU
    
    Tested: on DVT Mt.jade board
    
    1. Plug-on the CHICONY PSU
    2. Check information of PSU
     - Check the dbus object for new PSU information available:
           /xyz/openbmc_project/inventory/system/chassis/motherboard/powersupplyX
     - Check WebUI to verify the information

--- meta-ampere/meta-jade/recipes-ampere/inventory/psu-inventory-update/psu-inventory-update.sh
+++ meta-ampere/meta-jade/recipes-ampere/inventory/psu-inventory-update/psu-inventory-update.sh
@@ -16,8 +16,12 @@ INVENTORY_SERVICE='xyz.openbmc_project.Inventory.Manager'
 INVENTORY_OBJECT='/xyz/openbmc_project/inventory'
 INVENTORY_PATH='xyz.openbmc_project.Inventory.Manager'
 OBJECT_PATH="/system/chassis/motherboard/powersupply$1"
-IVENTORY_IFACE="xyz.openbmc_project.Inventory.Item"
 DRIVER_PATH="/sys/bus/i2c/drivers/pmbus/"
+manufacturer_name=''
+product_name=''
+product_partnumber=''
+product_vernumber=''
+product_serialnumber=''
 
 pmbus_read() {
 
@@ -61,6 +65,69 @@ pmbus_read() {
 
   # Return a result string
   echo $string
+}
+
+fru_read() {
+
+  # Syntax: fru_read $1 $2
+  # Where:  $1 is I2C Channel
+  #         $2 is I2C Address
+
+  # The data format is compliant with the Intel IPMI v1.0 specification.
+
+  declare -i offset=0
+  declare -i len
+  # Clear bit #7 #6 for unspecified
+  declare -i mask_len=0x3f
+
+  # PRODUCT INFO AREA OFFSET at 0x4 offset
+  offset+=4
+  product_area=$(i2cget -f -y $1 $2 $offset)
+
+  # Go to the PRODUCT AREA (In multiples of 8 bytes)
+  offset=$((8*$product_area))
+
+  # Skip Header of The PRODUCT AREA
+  offset+=3
+
+  # Read MANUFACTURER'S NAME
+  len=$(i2cget -f -y $1 $2 $offset)
+  len=$(($len&$mask_len))
+  offset+=1
+  manufacturer_name=$( pmbus_read $1 $2 $offset $len)
+
+  offset+=$(($len))
+
+  # Read PRODUCT NAME
+  len=$(i2cget -f -y $1 $2 $offset)
+  len=$(($len&$mask_len))
+  offset+=1
+  product_name=$( pmbus_read $1 $2 $offset $len)
+
+  offset+=$(($len))
+
+  # Read PRODUCT PARTNUMBER
+  len=$(i2cget -f -y $1 $2 $offset)
+  len=$(($len&$mask_len))
+  offset+=1
+  product_partnumber=$( pmbus_read $1 $2 $offset $len)
+
+  offset+=$(($len))
+
+  # Read PRODUCT VERSION
+  len=$(i2cget -f -y $1 $2 $offset)
+  len=$(($len&$mask_len))
+  offset+=1
+  product_vernumber=$( pmbus_read $1 $2 $offset $len)
+
+  offset+=$(($len))
+
+  # Read PRODUCT SERIAL NUMBER
+  len=$(i2cget -f -y $1 $2 $offset)
+  len=$(($len&$mask_len))
+  offset+=1
+  product_serialnumber=$( pmbus_read $1 $2 $offset $len)
+
 }
 
 update_inventory() {
@@ -109,22 +176,13 @@ fi
 
 # Check if the powersupply present
 if [ -e ${DRIVER_PATH}${driver_name} ]; then
-  result=$( pmbus_read $i2c_chan $i2c_addr 0x24 12)
-  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "Manufacturer" "s" $result
-
-  result=$( pmbus_read $i2c_chan $i2c_addr 0x31 36)
-  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "Model" "s" $result
-
-  result=$( pmbus_read $i2c_chan $i2c_addr 0x56 16)
-  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "PartNumber" "s" $result
-
-  result=$( pmbus_read $i2c_chan $i2c_addr 0x78 14)
-  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "SerialNumber" "s" $result
+  fru_read $i2c_chan $i2c_addr
 else
   echo "WARNING: The powersupply$1 is not present"
-  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "Manufacturer" "s" ""
-  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "Model" "s" ""
-  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "PartNumber" "s" ""
-  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "SerialNumber" "s" ""
 fi
+
+  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "Manufacturer" "s" $manufacturer_name
+  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "Model" "s" $product_name
+  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "PartNumber" "s" $product_partnumber
+  update_inventory $1 "xyz.openbmc_project.Inventory.Decorator.Asset" 1 "SerialNumber" "s" $product_serialnumber
 
