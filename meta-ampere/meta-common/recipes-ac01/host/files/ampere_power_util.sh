af42ccba55457099b2fe0c3ad77087908c900c90
Thu B Nguyen <tbnguyen@amperecomputing.com>
2021-07-05 13:32:26 +0700

    meta-ampere: mtjade: refactor host power control
    Refactor the Mt.Jade Host power control operations to support Altra CPU
    following the Altra SoC BMC Interface specification which still
    satisfying the OpenBMC power control framework.
    
    Tested: check the below cases and ensure Host state report correctly:
     1. Run "reboot" from Host Linux. Check the Host is graceful shutdown,
        then SYS_RESET is asserted and the Host is booted.
     2. Run "shutdown" from Host Linux. Check the Host is graceful shutdown
        then the chassis power is turned OFF.
     3. Run "ipmitool chassis power soft". Check the Host is graceful
        shutdown
     4. Execute Redfish GracefulShutdown. Check the Host is graceful
        shutdown then the chassis power is turned OFF.
     5. Execute Redfish GracefulRestart. Check the Host is graceful shutdown
        then turned ON.
     6. Check other IPMI chassis power and Redfish Reset actions work.
    
    Signed-off-by: Thu B Nguyen <tbnguyen@amperecomputing.com>

--- meta-ampere/meta-common/recipes-ac01/host/files/ampere_power_util.sh
+++ meta-ampere/meta-common/recipes-ac01/host/files/ampere_power_util.sh
@@ -1,43 +1,7 @@
 #!/bin/bash
 # Usage of this utility
-function set_gpio_active_low() {
-  if [ $# -ne 2 ]; then
-    echo "set_gpio_active_low: need both GPIO# and initial level";
-    return;
-  fi
-
-  if [ ! -d /sys/class/gpio/gpio$1 ]; then
-    echo $1 > /sys/class/gpio/export
-  fi
-  echo $2 > /sys/class/gpio/gpio$1/direction
-
-  if [ -d /sys/class/gpio/gpio$1 ]; then
-    echo $1 > /sys/class/gpio/unexport
-  fi
-}
-
-GPIO_OCP_AUX_PWREN=139
-GPIO_OCP_MAIN_PWREN=140
-GPIO_SPI0_PROGRAM_SEL=226
-GPIO_SPI0_BACKUP_SEL=227
-
-GPIO_BASE=$(cat /sys/class/gpio/gpio*/base)
-
 function usage() {
-  echo "usage: power-util mb [on|status|cycle|reset|graceful_reset|force_reset]";
-}
-
-power_off() {
-  echo "power_off"
-}
-
-power_on() {
-  echo "Powering on Server $2"
-  set_gpio_active_low $((${GPIO_BASE} + ${GPIO_OCP_AUX_PWREN})) high
-  set_gpio_active_low $((${GPIO_BASE} + ${GPIO_OCP_MAIN_PWREN})) high
-  set_gpio_active_low $((${GPIO_BASE} + ${GPIO_SPI0_PROGRAM_SEL})) high
-  set_gpio_active_low $((${GPIO_BASE} + ${GPIO_SPI0_BACKUP_SEL})) low
-  busctl set-property xyz.openbmc_project.State.Chassis /xyz/openbmc_project/state/chassis0 xyz.openbmc_project.State.Chassis RequestedPowerTransition s xyz.openbmc_project.State.Chassis.Transition.On
+  echo "usage: power-util mb [status|reset|force_reset]";
 }
 
 power_status() {
@@ -49,56 +13,14 @@ power_status() {
   fi
 }
 
-power_reset() {
-  echo "Reset on server $2"
-  busctl set-property xyz.openbmc_project.State.Host /xyz/openbmc_project/state/host0 xyz.openbmc_project.State.Host RequestedHostTransition s xyz.openbmc_project.State.Host.Transition.Reboot
-}
-
-timestamp() {
-  date +"%s" # current time
-}
-
 shutdown_ack() {
   if [ -f "/run/openbmc/host@0-softpoweroff" ]; then
     echo "Receive shutdown ACK triggered after softportoff the host."
     touch /run/openbmc/host@0-softpoweroff-shutdown-ack
   else
     echo "Receive shutdown ACK triggered"
-    power_off
-
-    current_time="$(timestamp)"
-    if [ -f "/run/openbmc/host@0-shutdown-req-time" ]; then
-      request_time="$(cat /run/openbmc/host@0-shutdown-req-time)"
-      # shutdow request must be done in 30s
-      if [[ "$current_time" -lt "$request_time + 30" ]]; then
-        power_off
-        sleep 5s
-        if [ -f "/run/openbmc/host@0-graceful-reset-time" ]; then
-            req_reset="$(cat /run/openbmc/host@0-graceful-reset-time)"
-            if [[ "$current_time" -lt "$req_reset + 45" ]]; then
-              echo "powering on because of reset request"
-              power_on
-              rm -rf "/run/openbmc/host@0-graceful-reset-time"
-            fi
-        fi
-      fi
-      rm -rf "/run/openbmc/host@0-shutdown-req-time"
-    fi
-  fi
-}
-
-graceful_shutdown() {
-  # the host@0-request created by ipmi, to indicate shutdown immediately
-  if [ -f "/run/openbmc/host@0-request" ]; then
-    echo "shutdown host immediately"
-    power_off
-  else
-    echo "Triggering graceful shutdown"
-    echo "$(timestamp)" > "/run/openbmc/host@0-shutdown-req-time"
-    gpioset -l 0 49=1
-    sleep 1s
-    gpioset -l 0 49=0
-    sleep 30s
+    sleep 3
+    systemctl start obmc-chassis-poweroff@0.target
   fi
 }
 
@@ -129,12 +51,27 @@ soft_off() {
   echo 0
 }
 
-graceful_reset() {
-  echo "$(timestamp)" > "/run/openbmc/host@0-graceful-reset-time"
-  graceful_shutdown
-}
-
 force_reset() {
+  if [ -f "/run/openbmc/host@0-softpoweroff" ]; then
+    # In graceful host reset, after trigger os shutdown,
+    # the phosphor-state-manager will call force-warm-reset
+    # in this case the force_reset should wait for shutdown_ack from host
+    cnt=30
+    while [ $cnt -gt 0 ];
+    do
+      if [ -f "/run/openbmc/host@0-softpoweroff-shutdown-ack" ]; then
+        break
+      fi
+      echo "Waiting for shutdown-ack count down $cnt"
+      sleep 1
+      cnt=$((cnt - 1))
+    done
+    # The host OS is failed to shutdown
+    if [ $cnt == 0 ]; then
+      echo "Shutdown-ack time out after 30s."
+      exit 0
+    fi
+  fi
   echo "Triggering sysreset pin"
   gpioset -l 0 91=1
   sleep 1
@@ -169,32 +106,7 @@ if [ ! -d "/run/openbmc/" ]; then
   mkdir -p "/run/openbmc/"
 fi
 
-if [ $2 = "on" ]; then
-  if [ $(power_status) == "off" ]; then
-    rm -rf /run/openbmc/chassis@*-on
-    rm -rf /run/openbmc/host@*-on
-    power_on
-  fi
-elif [ $2 == "cycle" ]; then
-  if [ $(power_status) == "on" ]; then
-    echo "Powering off server"
-    power_off
-    sleep 20s
-    power_on
-  else
-    echo "Host is already off, do nothing"
-  fi
-elif [ $2 == "reset" ]; then
-  if [ $(power_status) == "on" ]; then
-    power_reset
-  else
-    echo "ERROR: Server not powered on"
-  fi
-elif [ $2 == "graceful_reset" ]; then
-  if [ $(power_status) == "on" ]; then
-      graceful_reset
-  fi
-elif [ $2 == "shutdown_ack" ]; then
+if [ $2 == "shutdown_ack" ]; then
   shutdown_ack
 elif [ $2 == "status" ]; then
   power_status
--- meta-ampere/meta-common/recipes-phosphor/state/phosphor-state-manager_%.bbappend
+++ meta-ampere/meta-common/recipes-phosphor/state/phosphor-state-manager_%.bbappend
@@ -13,14 +13,3 @@ AMPERE_RESET_HOST_CHECK_TMPL = "ampere-reset-host-check@.service"
 do_install_append() {
     install -m 0644 ${WORKDIR}/${AMPERE_RESET_HOST_CHECK_TMPL} ${D}${systemd_unitdir}/system/phosphor-reset-host-check@.service
 }
-
-pkg_postinst_${PN}-obmc-targets_append() {
-    rm "$D$systemd_system_unitdir/obmc-host-warm-reboot@0.target.requires/xyz.openbmc_project.Ipmi.Internal.SoftPowerOff.service"
-    rm "$D$systemd_system_unitdir/obmc-host-warm-reboot@0.target.requires/obmc-host-force-warm-reboot@0.target"
-
-    rm "$D$systemd_system_unitdir/obmc-host-reboot@0.target.requires/phosphor-reboot-host@0.service"
-    rm "$D$systemd_system_unitdir/obmc-host-reboot@0.target.requires/obmc-host-shutdown@0.target"
-
-    rm "$D$systemd_system_unitdir/obmc-host-force-warm-reboot@0.target.requires/obmc-host-stop@0.target"
-    rm "$D$systemd_system_unitdir/obmc-host-force-warm-reboot@0.target.requires/phosphor-reboot-host@0.service"
-}
