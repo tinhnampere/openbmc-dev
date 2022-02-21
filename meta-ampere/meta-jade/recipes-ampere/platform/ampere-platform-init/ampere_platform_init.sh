2a86f09e36d96f11b73fb550fcefd56bdca1c408
Thang Q. Nguyen <thang@os.amperecomputing.com>
2022-02-21 06:22:26 +0000

    meta-ampere: use RebootCause to prevent power policy on BMC reboots
    With LastRebootCause and watchdog driver support setting LastRebootCause
    with the BMC reset reason, we no longer need to patch u-boot to have
    restartcause via kernel command line.
    This commit removes the u-boot patch and change to use LastRebootCause
    to prevent power policy applied when BMC is rebooted.
    
    Tested:
    1. Set power policy to always-on and turn OFF the Host.
    2. Reboot BMC and check if the Host is not turned ON.
    3. Do A/C power and check if the Host is turned ON after BMC boots
    complete.
    
    Signed-off-by: Thang Q. Nguyen <thang@os.amperecomputing.com>

--- meta-ampere/meta-jade/recipes-ampere/platform/ampere-platform-init/ampere_platform_init.sh
+++ meta-ampere/meta-jade/recipes-ampere/platform/ampere-platform-init/ampere_platform_init.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 
+# shellcheck source=/dev/null
 source /usr/sbin/gpio-lib.sh
 source /usr/sbin/gpio-defs.sh
 
 # Configure to boot from MAIN SPI-HOST
-gpio_configure_output $SPI0_BACKUP_SEL 0
+gpio_configure_output "$SPI0_BACKUP_SEL" 0
 
-gpio_configure_input $S0_I2C9_ALERT_L
-gpio_configure_input $S1_I2C9_ALERT_L
-gpio_configure_input $GPIO_BMC_VGA_FRONT_PRES_L
-gpio_configure_input $GPIO_S0_VRHOT_L
-gpio_configure_input $GPIO_S1_VRHOT_L
-gpio_configure_output $BMC_VGA_SEL 1
-
-# Set BMC_READY to indicate BMC is ready
-gpio_configure_output $BMC_READY 1
+gpio_configure_input "$S0_I2C9_ALERT_L"
+gpio_configure_input "$S1_I2C9_ALERT_L"
+gpio_configure_input "$GPIO_BMC_VGA_FRONT_PRES_L"
+gpio_configure_input "$GPIO_S0_VRHOT_L"
+gpio_configure_input "$GPIO_S1_VRHOT_L"
+gpio_configure_output "$BMC_VGA_SEL" 1
 
 # =======================================================
 # Below GPIOs are controlled by other services so just
 # initialize in A/C power only.
-cmdline=`cat /proc/cmdline`
-if [[ $cmdline == *resetreason=power* ]]; then
-	gpio_configure_output $BMC_GPIOR2_EXT_HIGHTEMP_L 1
-	gpio_configure_output $GPIO_BMC_VR_PMBUS_SEL_L 1
-	gpio_configure_output $GPIO_BMC_I2C6_RESET_L 1
+bootstatus=$(cat /sys/class/watchdog/watchdog0/bootstatus)
+if [ "$bootstatus" == '32' ]; then
+	gpio_configure_output "$BMC_GPIOR2_EXT_HIGHTEMP_L" 1
+	gpio_configure_output "$GPIO_BMC_VR_PMBUS_SEL_L" 1
+	gpio_configure_output "$GPIO_BMC_I2C6_RESET_L" 1
 
 	# Initialize OCP register
-	gpio_configure_output $OCP_AUX_PWREN 1
-	gpio_configure_output $OCP_MAIN_PWREN 0
+	gpio_configure_output "$OCP_MAIN_PWREN" 0
 
 	# Configure SPI-NOR/EEPROM switching
-	gpio_configure_output $SPI0_PROGRAM_SEL 0
-	gpio_configure_output $BMC_I2C_BACKUP_SEL 1
-	gpio_configure_output $SPI0_BACKUP_SEL 0
+	gpio_configure_output "$SPI0_PROGRAM_SEL" 0
+	gpio_configure_output "$BMC_I2C_BACKUP_SEL" 1
+	gpio_configure_output "$SPI0_BACKUP_SEL" 0
 
 	# Initialize BMC_SYS_PSON_L, SHD_REQ_L, BMC_SYSRESET_L
-	gpio_configure_output $SYS_PSON_L 1
-	gpio_configure_output $S0_SHD_REQ_L 1
-	gpio_configure_output $S0_SYSRESET_L 1
-	gpio_configure_output $S1_SYSRESET_L 1
+	gpio_configure_output "$SYS_PSON_L" 1
+	gpio_configure_output "$S0_SHD_REQ_L" 1
+	gpio_configure_output "$S0_SYSRESET_L" 1
+	gpio_configure_output "$S1_SYSRESET_L" 1
 
 	# RTC Lock, SPECIAL_BOOT
-	gpio_configure_output $RTC_LOCK 0
-	gpio_configure_output $S0_SPECIAL_BOOT 0
-	gpio_configure_output $S1_SPECIAL_BOOT 0
+	gpio_configure_output "$RTC_LOCK" 0
+	gpio_configure_output "$S0_SPECIAL_BOOT" 0
+	gpio_configure_output "$S1_SPECIAL_BOOT" 0
 fi
--- meta-ampere/meta-jade/recipes-bsp/u-boot/u-boot-aspeed_%.bbappend
+++
@@ -1,5 +0,0 @@
-FILESEXTRAPATHS:append:mtjade := "${THISDIR}/${PN}:"
-
-SRC_URI += " \
-            file://0001-aspeed-support-passing-system-reset-status-to-kernel.patch \
-           "
