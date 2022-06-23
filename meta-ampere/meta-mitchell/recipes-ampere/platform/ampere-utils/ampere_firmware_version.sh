ba7a177e5d999a9f8d249749805b1d5c26decd8a
Hieu Huynh <hieuh@os.amperecomputing.com>
2022-06-23 08:59:54 +0000

    meta-ampere:mtmitchell: Supports shell script for CPLD firmware version report
    Support a shell script named ampere_firmware_version.sh to report
    version for components on the system(Motherboard CPLD, Backplane CPLD,...)
    Syntax:
    - Get Motherboard CPLD firmware revision
    $ ampere_firmware_version.sh mb_cpld
    
    - Get Backplane CPLD firmware revision
    $ ampere_firmware_version.sh bp_cpld <id>
    
    Tested:
    1. Get MB CPLD firmware revision
       $ ampere_firmware_version.sh mb_cpld
         MB CPLD
         CPLD Version: 22060268
         CPLD DeviceID: 612BE043
    2. Get Backplane CPLD firmware revision
       $ ampere_firmware_version.sh bp_cpld 1
         Front Backplane 1 CPLD
         CPLD Version: 13
         CPLD DeviceID:  59  5A  42  42  30  31  36  35  30  31  30  35
    
    Signed-off-by: Hieu Huynh <hieuh@os.amperecomputing.com>

---
+++ meta-ampere/meta-mitchell/recipes-ampere/platform/ampere-utils/ampere_firmware_version.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Helper script to report firmware version for components on the system (MB CPLD, Backplane CPLD, …)
+# Author : Hieu Huynh (hieu.huynh@amperecomputing.com)
+#
+# Get MB CPLD firmware revision:
+#    ampere_firmware_version.sh mb_cpld
+#
+# Get Backplane CPLD firmware revision:
+#    ampere_firmware_version.sh bp_cpld <id>
+#    <id>: 1 for Front Backplane 1
+#          2 for Front Backplane 2
+#          3 for Front Backplane 3
+#          4 for Rear Backplane 1
+#          5 for Rear Backplane 2
+
+# shellcheck disable=SC2046
+
+do_mb_cpld_firmware_report() {
+	echo "MB CPLD"
+	ampere_cpldupdate_jtag -v
+	ampere_cpldupdate_jtag -i
+}
+
+do_bp_cpld_firmware_report() {
+	BP_ID=$1
+	if [[ $BP_ID == 1 ]]; then
+		echo "Front Backplane 1 CPLD"
+		ampere_cpldupdate_i2c -b 82 -s 0x40 -t 2 -v
+		ampere_cpldupdate_i2c -b 82 -s 0x40 -t 2 -i
+	elif [[ $BP_ID == 2 ]]; then
+		echo "Front Backplane 2 CPLD"
+		ampere_cpldupdate_i2c -b 84 -s 0x40 -t 2 -v
+		ampere_cpldupdate_i2c -b 84 -s 0x40 -t 2 -i
+	elif [[ $BP_ID == 3 ]]; then
+		echo "Front Backplane 3 CPLD"
+		ampere_cpldupdate_i2c -b 86 -s 0x40 -t 2 -v
+		ampere_cpldupdate_i2c -b 86 -s 0x40 -t 2 -i
+	elif [[ $BP_ID == 4 ]]; then
+		echo "Rear Backplane 1 CPLD"
+		ampere_cpldupdate_i2c -b 87 -s 0x40 -t 2 -v
+		ampere_cpldupdate_i2c -b 87 -s 0x40 -t 2 -i
+	elif [[ $BP_ID == 5 ]]; then
+		echo "Rear Backplane 2 CPLD"
+		ampere_cpldupdate_i2c -b 89 -s 0x40 -t 2 -v
+		ampere_cpldupdate_i2c -b 89 -s 0x40 -t 2 -i
+	fi
+}
+
+if [ $# -eq 0 ]; then
+	echo "Usage:"
+	echo "  - Get MB CPLD firmware revision"
+	echo "     $(basename "$0") mb_cpld"
+	echo "  - Get Backplane CPLD firmware revision"
+	echo "     $(basename "$0") bp_cpld <id>"
+	echo "    <id>:"
+	echo "        1 - FrontBP1"
+	echo "        2 - FrontBP2"
+	echo "        3 - FrontBP3"
+	echo "        4 - RearBP1"
+	echo "        5 - RearBP2"
+	exit 0
+fi
+
+TYPE=$1
+ID=$2
+
+if [[ $TYPE == "mb_cpld" ]]; then
+	do_mb_cpld_firmware_report
+elif [[ $TYPE == "bp_cpld" ]]; then
+	if [ -z "$ID" ]; then
+		echo "Please choose backplanes id: 1 - FrontBP1, 2 - FrontBP2, 3 - FrontBP3, 4 - FrontBP4, 5 - FrontBP5"
+		exit 0
+	elif [[ "$ID" -ge "1" ]] && [[ "$ID" -le "5" ]]; then
+		do_bp_cpld_firmware_report "$ID"
+	else
+		echo "Backplanes id invalid"
+	fi
+fi
+
+exit 0
