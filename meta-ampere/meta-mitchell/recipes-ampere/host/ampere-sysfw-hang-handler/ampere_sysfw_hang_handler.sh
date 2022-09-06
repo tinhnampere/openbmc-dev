68c2ec8da57f3073838d393ef33565f7edc4ec70
Thu Nguyen <thu@os.amperecomputing.com>
2022-09-06 14:32:57 +0700

    meta-ampere: mtmitchell: update sysfw hang detection
    Update ampere sysfw hang detection to use ampere host on target.
    
    Tested:
    1. Reboot BMC with the host is on. Hang detection script starts when
    the host is already on.
    2. Power off the host. The script should stop.
    3. Power on the host. The script should start when the host is already
    on.
    4. Reset the host. The script should stop when the action is triggered,
    and start when the host is already on.
    
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>

--- meta-ampere/meta-mitchell/recipes-ampere/host/ampere-sysfw-hang-handler/ampere_sysfw_hang_handler.sh
+++ meta-ampere/meta-mitchell/recipes-ampere/host/ampere-sysfw-hang-handler/ampere_sysfw_hang_handler.sh
@@ -19,45 +19,26 @@ function reset_system()
 	ipmitool chassis power reset
 }
 
-s0_last_hb_state=0; cnt=-1;
-last_host_state=0
-sleep_times=0
+s0_last_hb_state=0
+cnt=-1
 while true
 do
-    if [ "$sleep_times" == "20" ]; then
-        st=$(busctl get-property xyz.openbmc_project.State.Host \
-            /xyz/openbmc_project/state/host0 xyz.openbmc_project.State.Host \
-            CurrentHostState | cut -d"." -f6)
-        if [ "$st" == "Running\"" ]; then
-            last_host_state=1
-        else
-            last_host_state=0
-        fi
-        sleep_times=0
+    # Monitor heart beat GPIO value, GPIOF4 for Socket 0
+    s0_hb_state=$(gpio_name_get s0-heartbeat)
+    if [ "$s0_last_hb_state" != "$s0_hb_state" ]; then
+        cnt=0
+    else
+        cnt=$((cnt + 1))
     fi
-    sleep_times=$((sleep_times + 1))
-    if [ "$last_host_state" == "1" ]; then
-        # Monitor heart beat GPIO value, GPIOF4 for Socket 0
-        s0_hb_state=$(gpio_name_get s0-heartbeat)
 
-        if [ "$s0_last_hb_state" != "$s0_hb_state" ]; then
-            cnt=0
-        else
-            cnt=$((cnt + 1))
-        fi
-
-        if [ "$cnt" -ge 6 ]; then
-            state=$(busctl get-property org.openbmc.control.Power \
-                /org/openbmc/control/power0 org.openbmc.control.Power \
-                pgood | cut -d' ' -f2)
-            if [ "$state" == "1" ]; then
-                echo "Error: system firmware hang"
-                sel_trigger
-                reset_system
-            fi
-            s0_last_hb_state=0; cnt=-1
-        fi
-        s0_last_hb_state="$s0_hb_state"
+    if [ "$cnt" -ge 6 ]; then
+        echo "Error: system firmware hang"
+        sel_trigger
+        reset_system
+        exit 0
     fi
+    s0_last_hb_state="$s0_hb_state"
     sleep 0.5
 done
+
+exit 0
